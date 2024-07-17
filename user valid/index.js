2fcae32fe910a36095559cbebdcd72309f099885
Asingura Philip <asinguraphilip@gmail.com>
2024-07-17 22:17:07 +0300

    key events

--- user valid/index.js
+++ user valid/index.js
@@ -1,6 +1,37 @@
-const myarray = [{username:"philip", password: 12345 }
-    , {username:"dalton", password: 102030 }
-    ,{username:"milow", password: 34554 }
-]
+let userName = document.getElementById("username").value
 
-console.log(myarray[2]);
+
+
+let newDiv = document.createElement("div")
+document.body.prepend(newDiv)
+newDiv.textContent = "bear"
+newDiv.style.backgroundColor = "red"
+
+let button = document.createElement("button")
+document.body.append(button)
+button.textContent = "swap"
+button.setAttribute("id","swapButton")
+
+
+// let btn = document.getElementById("submit").addEventListener("submit",function(event){
+//     event.preventDefault()
+//     console.log(userName)
+//     console.log(event.type)
+// })
+
+
+button.addEventListener("click",function(){
+    newDiv.style.backgroundColor = "yellow"
+    newDiv.textContent = "battle star galactica"
+})
+
+
+button.addEventListener("mouseover",function(){
+    newDiv.style.backgroundColor = "blue"
+    newDiv.textContent = "beats"
+})
+
+button.addEventListener("mouseout",function(){
+    newDiv.style.backgroundColor = "red"
+    newDiv.textContent = "bear"
+})
